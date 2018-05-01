e36ea2d2c603a4396a7b8088c8a089f9d6d9a3d3
omniasaleh <engomnia2019@gmail.com>
2018-05-01 18:17:30 +0200

    Update createRoom.js

--- Routes/createRoom.js
+++ Routes/createRoom.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 const isFound = require('../server/utils/Functions.js').isFound;
 const createRoom = require('../server/utils/Functions.js').createRoom;
-//var authenticate = require('../midware.js');
-router.post('/create-room/:roomName',function(req,res){
-    var user = req.user;
+var token=require('../token.js');
+router.post('/create-room/:roomName',token,function(req,res){
+    var user = req.tok.body.name;
     var name = req.params.roomName;
     isTeacher(user,(flag) => {
       if (flag) {
