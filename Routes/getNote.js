ec8ea5a9cd7ae1dc58c13bc4387de8e1ce3ddb1c
omniasaleh <engomnia2019@gmail.com>
2018-05-03 17:52:10 +0200

    Update getNote.js

--- Routes/getNote.js
+++ Routes/getNote.js
@@ -2,15 +2,15 @@ const express=require('express');
 const JWT=require('jsonwebtoken');
 const config=require('../config.js');
 var request = require('request');
-var rooms=require('../Models/roomModel.js');
+var notesUser=require('../Models/notesUser.js');
 const router = express.Router();
 var token=require('../token.js');
 var auth;
 router.get('/:roomName',token,(req,res)=>{
-  var email=req.tok.body.email;
-  console.log(email);
+  var username=req.tok.username;
+  console.log(username);
   var roomName=req.params.roomName;
-  rooms.find({email:email,roomName:roomName},{_id:0},function(err,data){
+  notesUser.find({username:username,roomName:roomName},{_id:0,username:0,roomName:0},function(err,data){
     if(err)throw err;
     res.json(data);
   });
