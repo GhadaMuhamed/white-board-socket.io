0904dcce0ac0dd88c52d409b02382d13e248c967
omniasaleh <engomnia2019@gmail.com>
2018-05-05 14:25:38 +0200

    Create getchat.js

---
+++ Routes/getchat.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var findRoom= require('../server/utils/Functions.js').findRoom;
+var token = require('../token.js');
+var roomModel=require('../Models/roomModel.js');
+router.get('/:roomName',token,function(req,res){
+  var room = req.params.roomName;
+  var username = req.tok.username;
+  findRoom(room, (flag) => {
+    if (!flag)
+      res.json({"message" : "There isn't a room with that name"});
+  });
+  roomModel.find({"roomName" : room }, { "board" : 0, "message" : 1 ,"_id":0}, function(err,data){
+      if (err) throw err;
+      req.json(data);
+  });
+
+})
+
+module.exports = router;
